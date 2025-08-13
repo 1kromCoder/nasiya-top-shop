5aebf40f69a3017ab57cb020449db996977a3b1e
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-08-13 15:52:43 +0500

    added

--- src/debtor/debtor.service.ts
+++ src/debtor/debtor.service.ts
@@ -68,6 +68,7 @@ export class DebtorService {
             Seller: true,
             Debts: { include: { Payments: true } },
             Phones: true,
+            Imgs: true,
           },
           where,
           orderBy: { [sortBy]: order },
